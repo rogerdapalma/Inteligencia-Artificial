1ed894a40b46e6fe0b29ca6bb95a39c1640c05ee
Roger <94903084+rogerdapalma@users.noreply.github.com>
2023-11-16 08:01:43 -0300

    Update Util.py

--- Algoritimo Generico em Python/Util.py
+++ Algoritimo Generico em Python/Util.py
@@ -1,9 +1,11 @@
 import random
 
+# Classe utilitária com métodos estáticos para geração de palavras aleatórias
 class Util:
-    letras = "abcdefghijklmnopqrstuvxwyz"
-    tamanho = len(letras)
+    letras = "abcdefghijklmnopqrstuvxwyz"  # Alfabeto utilizado para gerar palavras aleatórias
+    tamanho = len(letras)  # Tamanho do alfabeto
 
     @staticmethod
     def gerar_palavra(n):
+        # Gera uma palavra aleatória de tamanho n, utilizando o alfabeto definido
         return ''.join(random.choice(Util.letras) for _ in range(n))
